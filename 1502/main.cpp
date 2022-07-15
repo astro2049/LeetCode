aa3106895a8c39750fb527451dba2b06e5183af3
Jerry Loi <45759373+astro2049@users.noreply.github.com>
2022-07-15 22:38:12 +0800

    1502. Can Make Arithmetic Progression From Sequence

---
+++ 1502/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "vector"
+#include "algorithm"
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int difference = arr[1] - arr[0];
+        for (int i = 2; i < arr.size(); i++) {
+            if (arr[i] - arr[i - 1] != difference) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
